b636e14e0cbfd5216d401444fa533f940f36383a
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2024-01-10 00:02:14 +0600

    test

--- product/rest/views/products.py
+++ product/rest/views/products.py
@@ -7,3 +7,5 @@ from product.rest.serializers.products import ProductSerializer
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    print("test git workflows")
