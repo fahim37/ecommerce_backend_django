9330b2b9322bd2c3d7162d08f34cc2e91f9bdb99
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2024-02-12 00:40:59 +0600

    Database reset command added

--- product/management/commands/core_init.py
+++ product/management/commands/core_init.py
@@ -1,11 +1,29 @@
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
+from django.db import connection
+from django.core.management import call_command
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+
+        with connection.cursor() as cursor:
+            cursor.execute("DROP SCHEMA public CASCADE;")
+            cursor.execute("CREATE SCHEMA public;")
+
+            # Apply migrations to create new tables
+            self.stdout.write(self.style.SUCCESS("Dropped all tables."))
+            self.stdout.write(self.style.SUCCESS("Applying migrations..."))
+            call_command("migrate")
+
+            self.stdout.write(self.style.SUCCESS("Database reset successfully."))
+
         User.objects.create_superuser(
             username="moshihud",
+            password="123456",
+        )
+        User.objects.create_superuser(
+            username="admin",
             password="123456",
         )
         self.stdout.write("Successfully created super user")
