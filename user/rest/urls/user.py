7bc9e0b8b502ea1acafda0f25338cb702888f5c0
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2024-02-23 14:21:11 +0600

    Import Statement Fixed

--- user/rest/urls/user.py
+++ user/rest/urls/user.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
-from user.rest.views import UserRegistrationView
+from user.rest.views.user import UserRegistrationView
+
 
 urlpatterns = [path("register/", UserRegistrationView.as_view(), name="registration")]
